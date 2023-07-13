ba8ca0fd5681c654bceef43099bbfc92844644d2
ShaliaRamiah <48883232+ShaliaRamiah@users.noreply.github.com>
2023-07-13 11:42:45 +0200

    Add files via upload

---
+++ parsingXML/src/main/java/com/mycompany/parsingXML/HelloWorld.java
@@ -0,0 +1,48 @@
+package com.mycompany.parsingXML;
+
+import java.io.IOException;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+public class HelloWorld {
+
+    public static void main(String[] args) {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        try {
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document doc = builder.parse("Example.xml");
+            
+            NodeList personList = doc.getElementsByTagName("person");
+            
+            for (int i = 0; i < personList.getLength(); i++){
+            Node p = personList.item(i);
+            
+            if (p.getNodeType() == Node.ELEMENT_NODE){
+                Element person = (Element) p;
+                String id = person.getAttribute("id");
+                NodeList nameList = person.getChildNodes();
+                
+                for(int j = 0; j < nameList.getLength(); j++){
+                    Node n = nameList.item(j);
+                    if(n.getNodeType() == Node.ELEMENT_NODE){
+                        Element name = (Element) n;
+                        System.out.print("Person " + id + ": " + name.getTagName() + " " 
+                                + name.getTextContent() + " ");
+                    }
+                }
+            }
+        }
+        } catch (ParserConfigurationException ex) {
+            ex.printStackTrace();
+        } catch (SAXException ex) {
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
